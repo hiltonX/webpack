0bf11592fc25b50e5f39928944c62aeed1c713ff
大姐姐 <wzxielulu@163.com>
2021-03-06 16:29:06 +0800

    补充笔记

--- src/index.js
+++ src/index.js
@@ -12,6 +12,13 @@
  * webpack ./src/index.js -o ./product --mode=production
  * 
  * 以./src/index.js为入口文件，将chunk输出到./product文件夹下，打包成main.js
+ * 
+ * 
+ * 
+ * 1. webpack只能处理js/json资源，不能处理css/img等其他资源
+ * 2. webpack将ES6语法编译成浏览器能识别的语法
+ * 3. 生产环境比开发环境多压缩一个js代码
+ * 
  */
 
 import data from './data.json'
